548111ae26a293df9d73caec362227748988e8fe
Marcus Brohede <a97marbr@gmail.com>
2018-10-06 16:42:56 +0200

    cleanup

--- dbsk4_2018/Controllers/HomeController.cs
+++ dbsk4_2018/Controllers/HomeController.cs
@@ -12,32 +12,9 @@ namespace dbsk4_2018.Controllers
     {
         public IActionResult Index()
         {
+            StudentsModel sm = new StudentsModel("wwwlab.iki.his.se-dbsk");
+            ViewBag.AllStudentsTable = sm.GetAllStudents();
             return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
