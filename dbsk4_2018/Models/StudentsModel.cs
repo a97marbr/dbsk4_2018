67c87b241ab8272fa37594e6eee10ba6722d8c68
Marcus Brohede <a97marbr@gmail.com>
2018-10-06 17:32:43 +0200

    seed code 2018

---
+++ dbsk4_2018/Models/StudentsModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace dbsk4_2018.Models
+{
+    public class StudentsModel
+    {
+        private string connectionString = "Server=localhost;Database=dbskdemo;User ID=ENTER_DB_USER;Password=ENTER_PASSWORD;Pooling=false;";
+
+        public StudentsModel(string connectionName)
+        {
+            //connectionString = System.Configuration.ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        public DataTable GetAllStudents()
+        {
+            MySqlConnection dbcon = new MySqlConnection(connectionString);
+            dbcon.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM Student;", dbcon);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "result");
+            DataTable studentsTable = ds.Tables["result"];
+            dbcon.Close();
+
+            return studentsTable;
+        }
+    }
+}
